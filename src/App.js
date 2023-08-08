d962a0719f4fe2d191808684f0b702d181e507cf
Trace Tessier <trace.tessier@gmail.com>
2023-08-08 11:52:49 -0400

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
@@ -7,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
