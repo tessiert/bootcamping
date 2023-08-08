439d32763d870552b93d5b7e4d7b23e9be9e9a09
Trace Tessier <trace.tessier@gmail.com>
2023-08-08 13:12:46 -0400

    Added Homepage display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,11 @@
+import { Container } from 'reactstrap';
+import DisplayList from '../features/display/DisplayList';
+
 const HomePage = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <Container>
+            <DisplayList />
+        </Container>
     )
 };
 
