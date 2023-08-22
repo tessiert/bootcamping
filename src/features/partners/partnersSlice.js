22b8a3ad602c5ca9e6457558cb4f453d826c72eb
Trace Tessier <trace.tessier@gmail.com>
2023-08-21 22:33:35 -0400

    Transitioned remaining slices to Redux

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,6 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { PARTNERS } from '../../app/shared/PARTNERS';
 
-export const selectAllPartners = () => PARTNERS;
+const initialState = {
+    partnersArray: PARTNERS
+};
 
-export const selectFeaturedPartner = () =>
-    PARTNERS.find((partner) => partner.featured);
+const partnersSlice = createSlice({
+    name: 'partners',
+    initialState
+});
+
+export const partnersReducer = partnersSlice.reducer;
+
+export const selectAllPartners = (state) =>
+    state.partners.partnersArray;
+
+export const selectFeaturedPartner = (state) =>
+    state.partners.partnersArray.find((partner) => partner.featured);
