d4f6f449edb302570bab630710bfc8989e975aa9
Trace Tessier <trace.tessier@gmail.com>
2023-08-28 15:57:49 -0400

    Added confirmation modal to contact form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,49 +3,44 @@ import SubHeader from '../components/SubHeader';
 import ContactForm from '../components/ContactForm';
 
 const ContactPage = () => {
-    return (
-        <Container>
-            <SubHeader current='Contact Us' />
-            <Row className='row-content align-items-center'>
-                <Col sm='4'>
-                    <h5>Our Address</h5>
-                    <address>
-                        1 Bootcamp Way
-                        <br />
-                        Deep River, CT 06417
-                        <br />
-                        U.S.A.
-                    </address>
-                </Col>
-                <Col>
-                    <a
-                        role='button'
-                        className='btn btn-link'
-                        href='tel:!18605551234'
-                    >
-                        <i className='fa fa-phone' /> 1-860-555-1234
-                    </a>
-                    <br />
-                    <a
-                        role='button'
-                        className='btn btn-link'
-                        href='mailto:fakeemail@fakeemail.co'
-                    >
-                        <i className='fa fa-envelope-o' /> info@BootCamping.com
-                    </a>
 
-                </Col>
-            </Row>
-            <Row className='row-content'>
-                <Col xs='12'>
-                    <h2>Send Us Your Feedback</h2>
-                    <hr />
-                </Col>
-                <Col md='10'>
-                    <ContactForm />>
-                </Col>
-            </Row>
-        </Container>
+    return (
+        <>
+            <Container>
+                <SubHeader current='Contact Us' />
+                <Row className='row-content align-items-center'>
+                    <Col sm='4'>
+                        <h5>Our Address</h5>
+                        <address>
+                            1 Bootcamp Way
+                            <br />
+                            Deep River, CT 06417
+                            <br />
+                            U.S.A.
+                        </address>
+                    </Col>
+                    <Col>
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:!18605551234'
+                        >
+                            <i className='fa fa-phone' /> 1-860-555-1234
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:fakeemail@fakeemail.co'
+                        >
+                            <i className='fa fa-envelope-o' /> info@BootCamping.com
+                        </a>
+
+                    </Col>
+                </Row>
+            </Container>
+            <ContactForm />
+        </>
     )
 };
 
